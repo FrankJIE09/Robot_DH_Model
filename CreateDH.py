2b5232745e29bc466684eb24d72f778000fe79b0
Frank <247625515@qq.com>
2023-06-27 15:49:09 +0800

    README

--- CreateDH.py
+++ CreateDH.py
@@ -1,29 +1,32 @@
+# 该文件是一个Python脚本，主要实现了一个六连杆机械臂的正逆运动学求解以及机械臂姿态可视化。
+# 其中用到了roboticstoolbox库来定义DH模型，并调用该库中的函数进行机械臂的正逆运动学求解，最后调用plot函数可视化机械臂姿态。
+# 程序中的init_T和theta均为机械臂的初始姿态和运动学参数，p为机械臂工作空间内的位置，q为机械臂的关节角度。该文件主要适用于控制六连杆机械臂运动的工程师。
+# Created by JieYu
 
 import roboticstoolbox as rtb
 from math import pi
 import numpy as np
 
-L1=0.320
-L3=0.975
+L1 = 0.320
+L3 = 0.975
 L5 = 0.200
 L6 = 0.887
 
 robot = rtb.DHRobot(
     [
-        rtb.RevoluteMDH(alpha=0,    a=0,    offset=0,   d=0 ),
-        rtb.RevoluteMDH(alpha=pi/2, a=L1,   offset=0,   d=0 ),
-        rtb.RevoluteMDH(alpha=0,    a=L3,   offset=0,   d=0 ),
-        rtb.RevoluteMDH(alpha=pi/2, a=L5,   offset=0,   d=L6),
-        rtb.RevoluteMDH(alpha=-pi/2,a=0,    offset=0,   d=0 ),
-        rtb.RevoluteMDH(alpha=pi/2, a=0,    offset=0,   d=0 )
-    ],name="six link")
+        rtb.RevoluteMDH(alpha=0, a=0, offset=0, d=0),
+        rtb.RevoluteMDH(alpha=pi / 2, a=L1, offset=0, d=0),
+        rtb.RevoluteMDH(alpha=0, a=L3, offset=0, d=0),
+        rtb.RevoluteMDH(alpha=pi / 2, a=L5, offset=0, d=L6),
+        rtb.RevoluteMDH(alpha=-pi / 2, a=0, offset=0, d=0),
+        rtb.RevoluteMDH(alpha=pi / 2, a=0, offset=0, d=0)
+    ], name="six link")
 
-init_T = np.array([0, pi/6, -pi/6, 0, 0, 0])
+init_T = np.array([0, pi / 6, -pi / 6, 0, 0, 0])
 robot.plot(init_T, movie='six link.png')
 # robot.teach() 		#示教模式
-theta = [0, pi/6, -pi/6, 0, 0, 0] 
-p = robot.fkine(theta)          #机械臂求正解
-print(p)	
-q = robot.ikine_LM(p)		#机械臂求逆解
+theta = [0, pi / 6, -pi / 6, 0, 0, 0]
+p = robot.fkine(theta)  # 机械臂求正解
+print(p)
+q = robot.ikine_LM(p)  # 机械臂求逆解
 print(q)
-input()
--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-安装必要库
-pip3 install roboticstoolbox-python
+# 六连杆机械臂运动控制程序
+# 项目描述
+该程序实现了对六连杆机械臂的运动控制，包括运动学参数的计算和机械臂姿态的可视化。
+# 运行环境
+>* Python 3.x
+>* roboticstoolbox库
 
-参考资料：
-https://blog.csdn.net/qq_26565435/article/details/91460988
+# 安装必要库
+> pip install roboticstoolbox-python
+# 使用方法
+
+>* 在运行该程序前，请确保安装了Python 3.x和roboticstoolbox库。
+>* 将程序中的init_T和theta设置为机械臂的初始姿态和运动学参数。
+>* 执行CreateDH.py，程序将自动计算机械臂的正逆运动学并将姿态可视化。
+# 仿真实物对照图
+<div style="display:flex;">
+    <img src="./依据ABB机械臂建立的仿真图.png" width="50%" style="margin-right:10px;">
+    <img src="./参照机械臂ABB.png" width="50%">
+</div>
+
+# 参考资料：
+![](./改进DH建模方式.png)
+
+https://github.com/petercorke/robotics-toolbox-python
+
