5a946cc341b6b797fb2dcd3139774e721166bb84
Frank <247625515@qq.com>
2023-06-27 17:37:27 +0800

    DH

--- CreateDH.py
+++ CreateDH.py
@@ -11,21 +11,21 @@ L1 = 0.320
 L3 = 0.975
 L5 = 0.200
 L6 = 0.887
-
+# 构造机械臂DH模型
 robot = rtb.DHRobot(
     [
-        rtb.RevoluteMDH(alpha=0, a=0, offset=0, d=0),
-        rtb.RevoluteMDH(alpha=pi / 2, a=L1, offset=0, d=0),
+        rtb.RevoluteMDH(alpha=0, a=0, offset=0, d=L1),
+        rtb.RevoluteMDH(alpha=-pi / 2, a=L1, offset=0, d=0),
         rtb.RevoluteMDH(alpha=0, a=L3, offset=0, d=0),
-        rtb.RevoluteMDH(alpha=pi / 2, a=L5, offset=0, d=L6),
-        rtb.RevoluteMDH(alpha=-pi / 2, a=0, offset=0, d=0),
-        rtb.RevoluteMDH(alpha=pi / 2, a=0, offset=0, d=0)
+        rtb.RevoluteMDH(alpha=-pi / 2, a=0, offset=0, d=L6),
+        rtb.RevoluteMDH(alpha=pi / 2, a=0, offset=0, d=0),
+        rtb.RevoluteMDH(alpha=-pi / 2, a=L5, offset=0, d=0)
     ], name="six link")
 
 init_T = np.array([0, pi / 6, -pi / 6, 0, 0, 0])
-robot.plot(init_T, movie='six link.png')
-# robot.teach() 		#示教模式
 theta = [0, pi / 6, -pi / 6, 0, 0, 0]
+robot.teach(theta) 		#示教模式
+robot.plot(init_T, movie='six link.png')
 p = robot.fkine(theta)  # 机械臂求正解
 print(p)
 q = robot.ikine_LM(p)  # 机械臂求逆解
